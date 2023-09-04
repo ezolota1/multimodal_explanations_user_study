92fbf7d574230475ce2d31b1481fb6b7648fe25b
ah96 <amarhalilovic96@gmail.com>
2023-09-04 13:46:26 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Define an array of predetermined links
+    const links = [
+        "https://example.com/link1",
+        "https://example.com/link2",
+        "https://example.com/link3"
+    ];
+
+    const linkElement = document.getElementById("link");
+    let currentIndex = 0;
+
+    // Function to update the link and redirect
+    function updateLinkAndRedirect() {
+        linkElement.href = links[currentIndex];
+        currentIndex = (currentIndex + 1) % links.length; // Cycle through the links
+    }
+
+    // Initial setup
+    updateLinkAndRedirect();
+
+    // Automatically redirect to the updated link after 2 seconds
+    setTimeout(function() {
+        window.location.href = linkElement.href;
+    }, 2000); // Redirect every 2 seconds (adjust as needed)
+});
