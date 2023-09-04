9fdcf3afc339508a26751c0cbab51a1319e374aa
ah96 <amarhalilovic96@gmail.com>
2023-09-04 14:09:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Define an array of predetermined links
-    const links = [
-        "https://www.avaz.ba/",
-        "https://www.klix.ba/",
-        "https://www.oslobodjenje.ba/"
-    ];
+// Define an array of predetermined links
+const links = [
+    "https://example.com/link1",
+    "https://example.com/link2",
+    "https://example.com/link3"
+];
 
-    const linkElement = document.getElementById("link");
-    let currentIndex = 0;
+const linkElement = document.getElementById("link");
+let userCount = 0;
 
-    // Function to update the link and redirect
-    function updateLinkAndRedirect() {
-        linkElement.href = links[currentIndex];
-        currentIndex = (currentIndex + 1) % links.length; // Cycle through the links
-    }
+// Function to update the link and redirect
+function updateLinkAndRedirect() {
+    const linkIndex = userCount % links.length;
+    linkElement.href = links[linkIndex];
+    userCount++;
+}
 
-    // Initial setup
-    updateLinkAndRedirect();
+// Initial setup
+updateLinkAndRedirect();
 
-    // Add a click event listener to the link
-    linkElement.addEventListener("click", function(e) {
-        e.preventDefault(); // Prevent the default link behavior
-        window.location.href = linkElement.href; // Redirect the user to the updated link
-    });
+// Add a click event listener to the link
+linkElement.addEventListener("click", function(e) {
+    // Prevent the default link behavior
+    e.preventDefault();
+
+    // Redirect the user to the updated link
+    window.location.href = linkElement.href;
 });
-
